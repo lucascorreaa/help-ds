392089fe35dea2f0b6bc962500528f1e344d6d32
Lucas <lucas.santo@einstein.br>
2023-05-22 14:47:53 -0300

    fix:

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,15 @@
-import React, { InputHTMLAttributes } from 'react'
+import React, { InputHTMLAttributes } from "react";
 
 export interface ButtonProps extends InputHTMLAttributes<HTMLButtonElement> {
-    label: string
+  label: string;
 }
 
 const ButtonWrapper: React.FC<ButtonProps> = (props) => {
+  return (
+    <div>
+      <button>{props.label}</button>
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <button>{props.label}</button>
-        </div>
-    )
-}
-
-export default ButtonWrapper
+export default ButtonWrapper;
--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -3,7 +3,11 @@ import React, { ImgHTMLAttributes } from "react";
 export interface IconProps extends ImgHTMLAttributes<HTMLImageElement> {}
 
 const Icon: React.FC<IconProps> = (props) => {
-  return <img src={props.src} alt={props.alt} {...props} />;
+  return (
+    <div>
+      <img src={props.src} alt={props.alt} {...props} />
+    </div>
+  );
 };
 
 export default Icon;
