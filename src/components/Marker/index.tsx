c9b889c3c3ece161bb7b1547c49866526e4d0af2
lucascorreaa <lucas.lca14@gmail.com>
2024-03-16 15:27:50 -0300

    feat: created component Marker

---
+++ src/components/Marker/index.tsx
@@ -0,0 +1,15 @@
+import React, { HtmlHTMLAttributes } from "react";
+import { MarkerStyled } from "./marker";
+
+export interface MarkerProps extends HtmlHTMLAttributes<HTMLHRElement> {
+  color: string;
+  borderColor: string
+  size: number;
+  border: boolean;
+}
+
+export const Marker: React.FC<MarkerProps> = ({ border, size, color, borderColor, ...props }) => {
+  return (
+    <MarkerStyled color={color} borderColor={borderColor} size={size} border={border} {...props} />
+  );
+};
---
+++ src/components/Marker/marker.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { MarkerProps } from ".";
+
+export const MarkerStyled = styled.hr<MarkerProps>`
+    background-color: ${({color}) => color}
+    border-radius: ${({size}) => size}
+    height: ${({size}) => size}
+    width: ${({size}) => size}
+    border: ${({border, borderColor}) => `4px ${border} ${borderColor}`}
+`
