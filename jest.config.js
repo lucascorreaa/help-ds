a31f195c9e4ad0414bcbbfff9bed7a4e1a2c296d
Lucas do Espirito Santo Correa <drt60337@MAC454846-M.local>
2024-03-20 19:12:56 -0300

    feat: added the jest to the project

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  testEnvironment: 'jsdom',
+};
---
+++ src/components/TextButton/text-button.spec.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { TextButton } from './index'
+
+describe('TextButton Component Test Unit', () => {
+
+  it('should render TextButton default', () => {
+    const { getByText } = render(
+        <TextButton label='Test label button' />
+    )
+
+    expect(getByText('Test label button')).toBeTruthy()
+  })
+  
+})
+
