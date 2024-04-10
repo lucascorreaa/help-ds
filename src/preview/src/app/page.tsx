c6e7c2212fa54f1274da1a72698b140f793d3342
lucascorreaa <lucas.lca14@gmail.com>
2024-04-09 21:59:39 -0300

    feat: added project preview in project

---
+++ src/preview/src/app/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import { Marquee } from '../../../index'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Marquee description='oi Next ' />
+    </main>
+  );
+}
