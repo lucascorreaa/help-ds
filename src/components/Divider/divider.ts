41f4a1280a0c79e777fe83fb019c30118d5c59b9
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-04-16 23:24:11 -0300

    refactor: Update Divider component styling

--- src/components/Divider/divider.ts
+++ src/components/Divider/divider.ts
@@ -1,22 +1,25 @@
 import styled from 'styled-components'
 import { DividerProps } from './index'
 
-const StyledHR = styled.hr<DividerProps>`
-  border-width: ${({ size }) => size ? size + "px" : "1px"};
-  border-style: ${({ styled }) => styled || "solid"};
-  border-color: ${({ color }) => color || "black"};
-  
+
+const StyledHR = styled.div<DividerProps>`
+  border: none;
+  width: ${({ fullWidth, width }) => (fullWidth === true && width !== undefined) ? `${width}px` : (fullWidth === true && width === undefined || fullWidth === false ? `100%` : `${width}`)}; // Verifica se fullWidth é true e width é undefined
+  ${({ size, styled, color }) => `
+    border-bottom-color: ${color};
+    border-bottom-width: ${size}px;
+    border-bottom-style: ${styled};
+    border-bottom-radius: 5px;
+  `}
   ${({ orientation }) => {
     if (orientation === "vertical") {
       return `
+        height: 0;
         transform: rotate(-90deg);
       `
-    } else {
-      return `
-        transform: rotate(90deg);;
-      `
-    }
+    } 
   }}
 `
+
 
 export default StyledHR
